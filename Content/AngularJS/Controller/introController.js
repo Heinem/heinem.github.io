c9bfa59edc8f3dc1e8c9434ccd5f1cea9a3871c0
unknown <heimoh@DESKTOP-GPB89M2.knowit.local>
2015-10-28 09:09:26 +0100

    Instagram Update

--- Content/AngularJS/Controller/introController.js
+++ Content/AngularJS/Controller/introController.js
@@ -1,8 +1,29 @@
 ﻿teamApp.controller('introController', ['$scope', '$routeParams', '$location', 'instagram',
  function introController($scope, $routeParams, $location, instagram) {
 
+     $scope.teamRunaway = "TEAM RUNAWAY";
+     
      $scope.go = function (path) {
          $location.path(path);
      };
 
+     $scope.showInstagram = function() {
+         $scope.instagram = true;
+         $scope.instaClass = "insta-hover";
+         console.log = "Inn";
+         $scope.teamRunaway = "#TeamRunawayNorway";
+
+     };
+
+     $scope.hideInstagram = function () {
+         $scope.instagram = false;
+         $scope.instaClass = "";
+         console.log = "out";
+         $scope.teamRunaway = "TEAM RUNAWAY";
+
+     };
+
+     instagram.getInstagramImg().success(function (data) {
+         $scope.pics = data.data;
+     });
  }]);
--- Content/AngularJS/app.js
+++ Content/AngularJS/app.js
@@ -4,23 +4,23 @@ teamApp.config(['$routeProvider','$httpProvider',
   function ($routeProvider, $httpProvider) {
       $routeProvider.
         when('/intro', {
-            templateUrl: 'Content/AngularJS/View/intro.html',
+            templateUrl: '/Content/AngularJS/View/intro.html',
             controller: 'introController'
         })
           .when('/bike', {
-              templateUrl: 'Content/AngularJS/View/bike.html',
+              templateUrl: '/Content/AngularJS/View/bike.html',
               controller: 'bikeController'
           })
           .when('/run', {
-              templateUrl: 'Content/AngularJS/View/run.html',
+              templateUrl: '/Content/AngularJS/View/run.html',
               controller: 'runController'
           })
           .when('/tri', {
-              templateUrl: 'Content/AngularJS/View/tri.html',
+              templateUrl: '/Content/AngularJS/View/tri.html',
               controller: 'triController'
           })
           .when('/instagram', {
-              templateUrl: 'Content/AngularJS/View/instagram.html',
+              templateUrl: '/Content/AngularJS/View/instagram.html',
               controller: 'instagramController'
           }).
             otherwise({
